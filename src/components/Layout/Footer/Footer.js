1b6c1b454190bc113ee59cf6b9e2064bd2b138ba
MichalAW <michal.wantaeuco@gmail.com>
2019-07-12 18:45:06 +0200

    proper version of react

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,11 +1,20 @@
-<section class="Home">
-  <div class="col-auto menu">
-    <ul>
-      <li><a href="#" class="active">Home</a></li>
-      <li><a href="#">About site</a></li>
-      <li><a href="#">Terms</a></li>
-      <li><a href="#">How to use it</a></li>
-      <li><a href="#">News</a></li>
-    </ul>
-  </div>
-</section>
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <section class="Home">
+        <div class="col-auto menu">
+          <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About site</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">How to use it</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+      </section>
+    )
+  }
+}
+
