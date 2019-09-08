5a80b2e8b1be8fcbf7e0aa7a61f88b6d806de0d7
Michał Wanta <michal.wanta@euco.pl>
2019-09-08 10:56:56 +0200

    fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Footer from '../Layout/Footer/Footer';
-import Midbar from '../Layout/Midbar/Midbar';
+import Midbar from '../Layout/MidBar/MidBar';
 import Topbar from '../Layout/Topbar/Topbar';
 import Main from '../Layout/Main/Main';
 import Sorter from '../Utils/Sorter/Sorter';
--- src/components/Layout/MidBar/MidBar.js
+++ src/components/Layout/MidBar/MidBar.js
@@ -11,7 +11,7 @@ import '../../../../node_modules/bootstrap/dist/css/bootstrap-reboot.min.css';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap-reboot.css';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-export default class Select extends Component {
+export default class Midbar extends Component {
 	constructor(props) {
 		super(props);
 		this.addProductToCart = this.addProductToCart.bind(this);
@@ -55,40 +55,36 @@ export default class Select extends Component {
 
 	render() {
 		return (
-			// <Route
-			// 	render={({ location }) => (
-					<section className="mid-bar">
-						<div className="company-claim">
-							<div className="container">
-								<div className="row align-items-center">
-									<div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 phone-number">
-										<p>
-											<i className="fas fa-mobile-alt"></i>
-											+48 665 631 399
-										</p>
-									</div>
-									<div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 text-center company-claim-logo">
-										<img src={logo} alt="Logo" />
-									</div>
-									<div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 cart">
-										<a href={"/cart/"}>
-											<div className="cart-box">
+			<section className="mid-bar">
+				<div className="company-claim">
+					<div className="container">
+						<div className="row align-items-center">
+							<div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 phone-number">
+								<p>
+									<i className="fas fa-mobile-alt"></i>
+									+48 665 631 399
+								</p>
+							</div>
+							<div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 text-center company-claim-logo">
+								<img src={logo} alt="Logo" />
+							</div>
+							<div className="col-sm-6 col-md-4 col-lg-4 col-xl-4 cart">
+								<a href={"/cart/"}>
+									<div className="cart-box">
 
-												<div className="cart-icon">
-													<i className="fas fa-shopping-basket"></i>
-												</div>
-												<div className="cart-counter">
-													{this.state.cartCount}
-												</div>
-											</div>
-										</a>
+										<div className="cart-icon">
+											<i className="fas fa-shopping-basket"></i>
+										</div>
+										<div className="cart-counter">
+											{this.state.cartCount}
+										</div>
 									</div>
-								</div>
+								</a>
 							</div>
 						</div>
-					</section>
-		// 		)}
-		// 	/>
+					</div>
+				</div>
+			</section>
 		)
 	}
 }
